0e816b0ca6e639a970bfba8c8f0408e14d73802f
wangjingyi <wangjingyi@tfzq.com>
2023-10-23 13:07:03 +0800

    5-4 集成图标组件

--- web/src/main.ts
+++ web/src/main.ts
@@ -3,9 +3,14 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import Antd from 'ant-design-vue';
-// import 'ant-design-vue/dist/antd.css';
 import 'ant-design-vue/dist/reset.css';
+import * as Icons from '@ant-design/icons-vue';
 
 const app = createApp(App)
+
+const icons:any = Icons;
+for (const i in icons) {
+    app.component(i, icons[i]);
+}
 
 app.use(store).use(router).use(Antd).mount('#app')
