d2387ef6b29be8fcbbba39caae739b2772c2e76d
wangjingyi <wangjingyi@tfzq.com>
2023-10-23 13:43:14 +0800

    5-5 修改axios的baseUrl读配置文件

--- web/src/views/HomeView.vue
+++ web/src/views/HomeView.vue
@@ -69,26 +69,14 @@ import axios from 'axios';
 
 const listData: Record<string, string>[] = [];
 
-for (let i = 0; i < 23; i++) {
-  listData.push({
-    href: 'https://www.antdv.com/',
-    title: `ant design vue part ${i}`,
-    avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
-    description:
-      'Ant Design, a design language for background applications, is refined by Ant UED Team.',
-    content:
-      'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
-  });
-}
-
 export default defineComponent({
   name: 'HomeView',
   setup() {
     console.log("setup")
     const ebooks = ref();
     onMounted(() => {
-      console.log("onMounted111")
-      axios.get(process.env.VUE_APP_SERVER + "/ebook/list").then((response) => {
+      console.log("onMounted123")
+      axios.get("/ebook/list").then((response) => {
         const data = response.data;
         ebooks.value = data.content
         console.log(response);
