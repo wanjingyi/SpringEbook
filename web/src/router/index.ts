8a8f6c8c27362069d441e113e2d63de77ecaf8b7
wangjingyi <wangjingyi@tfzq.com>
2023-10-23 15:28:07 +0800

    6-2 增加电子书管理页面。新页面开发，页面，路由，菜单

--- web/src/router/index.ts
+++ web/src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+// import About from '../views/About.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -10,10 +11,20 @@ const routes: Array<RouteRecordRaw> = [
   {
     path: '/about',
     name: 'about',
+    // component: About
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
+    // which is lazy-loaded when the route is visited.----懒加载
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: '/admin/ebook',
+    name: 'abminEbook',
+    // component: About
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.----懒加载
+    component: () => import(/* webpackChunkName: "about" */ '../views/admin/ebook.vue')
   }
 ]
 
